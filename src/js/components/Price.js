167b803ef096d7ffab9c3ab3697d5daae23c748a
Guille Paz <guillermo.paz@mercadolibre.com>
2016-08-01 16:40:39 -0300

    Update dependencies and eslint config.

--- src/js/components/Price.js
+++ src/js/components/Price.js
@@ -7,9 +7,11 @@ import React from 'react';
  * Component definition
  */
 const Price = props => (
-  <span className={"price-tag " + (props.className || '')} itemProp="offers" itemScope itemType="http://schema.org/Offer">
-    <meta itemProp="price" content={props.fraction + props.separator + props.cents}/>
-    <span className="price-tag-symbol" itemProp="priceCurrency" content={props.currency}>{props.symbol}</span>
+  <span className={`price-tag ${(props.className || '')}`} itemProp="offers" itemScope itemType="http://schema.org/Offer">
+    <meta itemProp="price" content={props.fraction + props.separator + props.cents} />
+    <span className="price-tag-symbol" itemProp="priceCurrency" content={props.currency}>
+      {props.symbol}
+    </span>
     <span className="price-tag-fraction">{props.fraction}</span>
     <span className="price-tag-decimal-separator">{props.separator}</span>
     <span className="price-tag-cents">{props.cents}</span>
@@ -26,7 +28,7 @@ Price.propTypes = {
   fraction: React.PropTypes.string.isRequired,
   separator: React.PropTypes.oneOf(['.', ',']).isRequired,
   cents: React.PropTypes.string.isRequired,
-  lineThrough: React.PropTypes.bool
+  lineThrough: React.PropTypes.bool,
 };
 
 /**
@@ -34,11 +36,11 @@ Price.propTypes = {
  */
 export default Price;
 
-
-
 // const content = props => (
 //   <div>
-//     <span className="price-tag-symbol" itemprop="priceCurrency" content={props.currency}>{props.symbol}</span>
+// <span className="price-tag-symbol" itemProp="priceCurrency" content={props.currency}>
+//   {props.symbol}
+// </span>
 //     <span className="price-tag-fraction">{props.fraction}</span>
 //     <span className="price-tag-decimal-separator">{props.separator}</span>
 //     <span className="price-tag-cents">{props.cents}</span>
@@ -50,8 +52,8 @@ export default Price;
 //  * Component definition
 //  */
 // const Price = props => (
-//   <span className={"price-tag " + (props.className || '')} itemprop="offers" itemscope itemtype="http://schema.org/Offer">
-//     <meta itemprop="price" content={props.fraction + props.separator + props.cents}/>
+//   <span className={"price-tag " + (props.className || '')} itemProp="offers" itemScope itemtType="http://schema.org/Offer">
+//     <meta itemProp="price" content={props.fraction + props.separator + props.cents}/>
 //     {
 //       props.lineThrough ?
 //         <del>{content(props)}</del>
